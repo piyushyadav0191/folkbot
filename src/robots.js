407c3204d3412e964434899012ec315cf29ac839
piyush yadav <84402719+piyushyadav0191@users.noreply.github.com>
2021-10-19 20:54:42 +0530

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'pizzaboy',
+    username: 'pizza',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'burger',
+    username: 'burgerboy',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'momo',
+    username: 'momoboy',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'tortella',
+    username: 'tortellaboy',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'paratha',
+    username: 'parathaboy',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'dosa',
+    username: 'dosaboy',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'sandwich  ',
+    username: 'sandwichboy',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'idli',
+    username: 'idliboy',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'rice',
+    username: 'riceboy',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'dalchawal',
+    username: 'dalchawalboy',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
